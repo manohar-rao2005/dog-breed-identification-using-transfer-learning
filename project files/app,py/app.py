578dc0f3c3f4fe1516603ec22770aad87bfa6f98
manohar-rao2005 <umanohar2005@gmail.com>
2026-02-19 19:34:42 +0530

    Add files via upload

---
+++ project files/app,py/app.py
@@ -0,0 +1,35 @@
+import numpy as np
+import os
+import tensorflow as tf
+from PIL import Image
+from flask import Flask, render_template, request,jsonify,url_for, redirect
+from tensorflow.keras.preprocessing.image import load_img,img_to_array
+app=Flask (_name__)
+model = tf.keras tf.keras.models.load_model('dogbreed.h5')
+@app.route('/')
+def index():
+    return render_template("index.html")
+@app.route('/predict')
+def predict():
+    return render_template("predict.html")
+@app.route('/output', methods=['GET', 'POST'])
+def output():
+    if request.method=='POST':
+        f=request.files['file']
+basepath=os.path.dirname(_file_)
+filepath=os.path.join(basepath,'uploads", f.filename)
+f.save(filepath)
+img=load_img(filepath, target_size=(224, 224))
+# Resize the image to the required size
+# Convert the image to an array and normalize it
+image_array = np.array(img)
+# Add a batch dimension
+image_array np.expand_dims (image_array, axis=0)
+# Use the pre-trained model to make a prediction
+pred-np.argmax(model.predict (image_array), axis-1)
+index=['affenpinscher', 'beagle', 'appenzeller', 'basset', 'bluetick', 'boxer', 'cairn', 'doberman', 'german_shepherd', 'golden_retriever']
+prediction = index[int (pred)]
+print("prediction")
+return render_template("output.html", predict prediction)
+if name main':
+app.run (debug=False, threaded False)
